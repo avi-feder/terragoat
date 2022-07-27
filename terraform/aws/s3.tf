323b22b2908e2ce91c62a90873277f35bb50b269
avi-feder <93920286+avi-feder@users.noreply.github.com>
2022-07-27 17:40:01 +0300

    Update s3.tf

--- terraform/aws/s3.tf
+++ terraform/aws/s3.tf
@@ -21,7 +21,10 @@ resource "aws_s3_bucket" "data" {
   })
 }
 
+
+
 resource "aws_s3_bucket_object" "data_object" {
+  
   bucket = aws_s3_bucket.data.id
   key    = "customer-master.xlsx"
   source = "resources/customer-master.xlsx"
@@ -41,6 +44,9 @@ resource "aws_s3_bucket_object" "data_object" {
 }
 
 resource "aws_s3_bucket" "financials" {
+  
+  
+  
   # bucket is not encrypted
   # bucket does not have access logs
   # bucket does not have versioning
@@ -93,7 +99,7 @@ resource "aws_s3_bucket" "data_science" {
   bucket = "${local.resource_prefix.value}-data-science"
   acl    = "private"
   versioning {
-    enabled = true
+    enabled =  true
   }
   logging {
     target_bucket = "${aws_s3_bucket.logs.id}"
