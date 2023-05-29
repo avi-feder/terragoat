7876f76d2d4b490faf501fbaffc2526b39336a67
avi-feder <93920286+avi-feder@users.noreply.github.com>
2023-05-29 18:16:32 +0300

    Update lambda.tf

--- terraform/aws/lambda.tf
+++ terraform/aws/lambda.tf
@@ -42,7 +42,6 @@ resource "aws_lambda_function" "analysis_lambda" {
 
   environment {
     variables = {
-      access_key = "AKIAIOSFODNN7EXAMPLE"
       secret_key = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
     }
   }
